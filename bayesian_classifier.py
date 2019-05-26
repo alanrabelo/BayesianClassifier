f6dbb13e48f49d7c2a44c7c8720566cda4703b31
alanrabelo <alanrabelo@icloud.com>
2019-05-26 19:58:37 -0300

    Adicionando Teste de acurácia

--- bayesian_classifier.py
+++ bayesian_classifier.py
@@ -1,10 +1,15 @@
 import random
 import numpy as np
+from scipy.stats import *
+import math
+from numpy.linalg import inv, det
 
 class Bayesian_classifier:
 
     averages = {}
     std = {}
+    priors = {}
+    covariances = {}
 
     def load_data(self, filename):
 
@@ -39,28 +44,32 @@ class Bayesian_classifier:
 
             return self.split_train_test()
 
-    def split_train_test(self, percentage=0.8):
+    def split_train_test(self, percentage=0.8, evaluate=0.1):
 
         shuffled_dataset = self.dataset
         random.shuffle(shuffled_dataset)
 
-        split_point = round(len(shuffled_dataset)*percentage)
+        split_point = round(len(shuffled_dataset)*(1-evaluate) * percentage)
+        evaluate_point = round(len(shuffled_dataset)*(1-evaluate))
 
         train = np.array(shuffled_dataset[:split_point])
-        test = np.array(shuffled_dataset[split_point:])
+        test = np.array(shuffled_dataset[split_point:evaluate_point])
+        evaluate = np.array(shuffled_dataset[evaluate_point:])
 
-        return train[:, :-1], train[:, -1], test[:, :-1], test[:, -1]
+        return train[:, :-1], train[:, -1], test[:, :-1], test[:, -1], evaluate[:, :-1], evaluate[:, -1]
 
     def fit(self, x_train, y_train):
 
         values = {}
+        number_of_variables = 0
 
         for index, x_value in enumerate(x_train):
+
+            number_of_variables = len(x_value)
 
             y_value = list(y_train)[index]
 
             if y_value in values.keys():
-
                 values[y_value].append(x_value)
             else:
                 values[y_value] = [x_value]
@@ -74,17 +83,106 @@ class Bayesian_classifier:
             average = sum(all_values)/len(all_values)
             self.averages[key] = average
 
+            self.priors[key] = len(all_values) / sum([len(w) for w in values.values()])
+
             # Cálculo das Variâncias
             for value in all_values:
                 var_res = sum((x_input - average) ** 2 for x_input in all_values) / len(all_values)
                 self.std[key] = var_res ** 0.5
 
+        # x_train = np.array(x_train)
+
+        from itertools import product
+
+        number_of_variables = len(x_train[0])
+        self.covariance = np.zeros((number_of_variables, number_of_variables))
+
+        combinations = [p for p in product(list(range(0, number_of_variables)), repeat=2)]
+
+        values_by_class = {}
+
+        for index, value in enumerate(x_train):
+
+            if y_train[index] in values_by_class:
+                values_by_class[y_train[index]].append(value)
+            else:
+                values_by_class[y_train[index]] = [value]
+
+
+
+        for key in values_by_class:
+
+            values = np.array(values_by_class[key])
+
+            covariance_matrix = np.zeros((number_of_variables, number_of_variables))
+
+            for x1, x2 in combinations:
+
+                var_x1_values = values[:, x1]
+                var_x2_values = values[:, x2]
+                x1_average = sum(var_x1_values)/len(var_x1_values)
+                x2_average = sum(var_x2_values)/len(var_x2_values)
+
+
+                covariance = (sum(var_x1_values * var_x2_values) / len(var_x1_values)) - (x1_average * x2_average)
+                covariance_matrix[x1][x2] = covariance
+            self.covariances[key] = covariance_matrix
 
         # cálculo da matriz de covariância
 
-        print("Averages will be here: \n %s" % str(self.averages))
 
-        print("Variances will be here: \n %s" % str(self.std))
+        # print("Averages will be here: \n %s" % str(self.averages))
+        # print("Priors will be here: \n %s" % str(self.priors))
+        # print("Variances will be here: \n %s" % str(self.std))
+        # print("CoVariances will be here: \n %s" % str(self.covariance))
+
+
+    # def prior(self, train_y):
+
+    def gaussian(self, x_test, cov, u1, pw=0.5):
+        u1_transp = u1.transpose()
+        x_test_transp = x_test.transpose()
+
+        return -0.5 * (np.dot(np.dot(x_test_transp, inv(cov)), x_test)) + \
+               0.5 * (np.dot(np.dot(x_test_transp, inv(cov)), u1)) + \
+               0.5 * (np.dot(np.dot(u1_transp, inv(cov)), x_test)) - \
+               0.5 * (np.dot(np.dot(u1_transp, inv(cov)), u1)) + math.log(pw) \
+               - (0.5 * math.log(2 * math.pi)) \
+               - (0.5 * math.log(det(cov)))
+
+    def evaluate(self, x_test, y_test):
+
+        number_of_elements = float(len(x_test))
+        acertos = 0
+        erros = 0
+
+        for index,value in enumerate(x_test):
+
+            y = self.predict(value)
+
+            if y == y_test[index]:
+                acertos += 1
+            else:
+                erros += 1
+
+        return acertos / number_of_elements
+
+
+    def predict(self, x):
+
+        max_probability = -math.inf
+        best_classe = 0
+
+        for possible_class in self.averages.keys():
+
+            probability = self.gaussian(x, self.covariances[possible_class], self.averages[possible_class], self.priors[possible_class])
+
+            if probability > max_probability:
+                max_probability = probability
+                best_classe = possible_class
+
+        return best_classe
+
 
 
 
--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from bayesian_classifier import Bayesian_classifier
 
 classifier = Bayesian_classifier()
 
-x_train, y_train, x_test, y_test = classifier.load_data('iris.data')
+x_train, y_train, x_test, y_test, x_evaluation, y_evaluation = classifier.load_data('iris.data')
 classifier.fit(x_train, y_train)
+
+accuracy = classifier.evaluate(x_test, y_test)
+
+print("O classificador bayesiano acertou %.2f%%" % (accuracy * 100))
