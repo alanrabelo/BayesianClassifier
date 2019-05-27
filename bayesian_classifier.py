95493ed36e7b4f935060b63b20bf3deaa652ff44
alanrabelo <alanrabelo@icloud.com>
2019-05-27 11:52:21 -0300

    Adicionando datasets e computando acurácia

--- bayesian_classifier.py
+++ bayesian_classifier.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.stats import *
 import math
 from numpy.linalg import inv, det
+import matplotlib.pyplot as plt
 
 class Bayesian_classifier:
 
@@ -11,17 +12,17 @@ class Bayesian_classifier:
     priors = {}
     covariances = {}
 
-    def load_data(self, filename):
+    def load_data(self, filename, add_evaluation_values=False):
 
         self.dataset = []
 
-        with open('iris.data') as data:
+        with open(filename) as data:
 
             categories = {}
 
             for line in data.readlines():
 
-                if len(line) < 10:
+                if len(line) < 5 or "?" in line:
                     continue
 
                 data_line = line.split(',')
@@ -42,9 +43,24 @@ class Bayesian_classifier:
                 self.dataset.append(data_empty)
 
 
-            return self.split_train_test()
+            if 'breast' in filename:
+                self.dataset = np.array(self.dataset)[:, 1:]
 
-    def split_train_test(self, percentage=0.8, evaluate=0.1):
+            normalized_dataset = np.array(self.dataset, dtype=float)
+
+            number_of_columns = len(self.dataset[0])
+            for column in range(number_of_columns - 1):
+                current_column = normalized_dataset[:, column]
+                max_of_column = float(max(current_column))
+                min_of_column = float(min(current_column))
+
+                for index in range(len(self.dataset)):
+                    value = self.dataset[index][column]
+                    self.dataset[index][column] = (float(value) - min_of_column) / (max_of_column - min_of_column)
+
+            return self.split_train_test_evaluate() if add_evaluation_values else self.split_train_test()
+
+    def split_train_test_evaluate(self, percentage=0.9, evaluate=0.1):
 
         shuffled_dataset = self.dataset
         random.shuffle(shuffled_dataset)
@@ -57,6 +73,18 @@ class Bayesian_classifier:
         evaluate = np.array(shuffled_dataset[evaluate_point:])
 
         return train[:, :-1], train[:, -1], test[:, :-1], test[:, -1], evaluate[:, :-1], evaluate[:, -1]
+
+    def split_train_test(self, percentage=0.8):
+
+        shuffled_dataset = self.dataset
+        random.shuffle(shuffled_dataset)
+
+        split_point = round(len(shuffled_dataset) * percentage)
+
+        train = np.array(shuffled_dataset[:split_point])
+        test = np.array(shuffled_dataset[split_point:])
+
+        return train[:, :-1], train[:, -1], test[:, :-1], test[:, -1]
 
     def fit(self, x_train, y_train):
 
@@ -114,7 +142,7 @@ class Bayesian_classifier:
 
             values = np.array(values_by_class[key])
 
-            covariance_matrix = np.zeros((number_of_variables, number_of_variables))
+            covariance_matrix = np.zeros((number_of_variables, number_of_variables), dtype=float)
 
             for x1, x2 in combinations:
 
@@ -140,30 +168,61 @@ class Bayesian_classifier:
     # def prior(self, train_y):
 
     def gaussian(self, x_test, cov, u1, pw=0.5):
+
         u1_transp = u1.transpose()
         x_test_transp = x_test.transpose()
+        det_cov = det(cov)
 
-        return -0.5 * (np.dot(np.dot(x_test_transp, inv(cov)), x_test)) + \
-               0.5 * (np.dot(np.dot(x_test_transp, inv(cov)), u1)) + \
-               0.5 * (np.dot(np.dot(u1_transp, inv(cov)), x_test)) - \
-               0.5 * (np.dot(np.dot(u1_transp, inv(cov)), u1)) + math.log(pw) \
-               - (0.5 * math.log(2 * math.pi)) \
-               - (0.5 * math.log(det(cov)))
+        try:
+            inv_cov = inv(cov)
+        except:
+            inv_cov = cov
 
-    def evaluate(self, x_test, y_test):
+        result = -0.5 * (np.dot(np.dot(x_test_transp, inv_cov), x_test)) + \
+               0.5 * (np.dot(np.dot(x_test_transp, inv_cov), u1)) + \
+               0.5 * (np.dot(np.dot(u1_transp, inv_cov), x_test)) - \
+               0.5 * (np.dot(np.dot(u1_transp, inv_cov), u1))
+
+
+        try:
+            log_det_cov = math.log(det_cov)
+        except:
+            log_det_cov = 0
+
+        return result - log_det_cov - (0.5 * math.log(2 * math.pi)) + math.log(pw)
+
+
+
+
+
+
+    def evaluate(self, x_test, y_test, conf_matrix=False):
 
         number_of_elements = float(len(x_test))
         acertos = 0
         erros = 0
+        confusion_matrix = {}
 
         for index,value in enumerate(x_test):
 
             y = self.predict(value)
+            desired = int(y_test[index])
+
+            if desired in confusion_matrix:
+                if y in confusion_matrix[desired]:
+                    confusion_matrix[desired][y] += 1
+                else:
+                    confusion_matrix[desired][y] = 1
+            else:
+                confusion_matrix[desired] = {y: 1}
 
             if y == y_test[index]:
                 acertos += 1
             else:
                 erros += 1
+
+        if conf_matrix:
+            print(confusion_matrix)
 
         return acertos / number_of_elements
 
@@ -175,6 +234,10 @@ class Bayesian_classifier:
 
         for possible_class in self.averages.keys():
 
+            cov = self.covariances[possible_class]
+            if len(cov) == 0:
+                continue
+
             probability = self.gaussian(x, self.covariances[possible_class], self.averages[possible_class], self.priors[possible_class])
 
             if probability > max_probability:
@@ -184,6 +247,50 @@ class Bayesian_classifier:
         return best_classe
 
 
+    def plot_decision_surface(self, filename, title):
+
+        # parameter_combination = list(itertools.combinations(range(len(list(X[0]))), 2))
+
+        x_train, y_train, x_test, y_test = self.load_data(filename)
+
+        x_train = x_train[:, :2]
+        x_test = x_test[:, :2]
+
+        self.fit(x_train, y_train)
+
+        clear_red = "#ffcccc"
+        clear_blue = "#ccffff"
+        clear_green = "#ccffcc"
+        clear_yellow = "#F5FAA9"
+        clear_pink = "#F9A9FA"
+        clear_orange = "#FBECD1"
+
+        colors = [clear_red, clear_blue, clear_green, clear_yellow, clear_pink, clear_orange]
+        strong_colors = ['red', 'blue', '#2ECC71', '#F9FF2D', '#FF2DF2', '#FFAE00']
+        number_of_points = 100
+
+        for i in range(0, number_of_points+1, 1):
+            for j in range(0, number_of_points+1, 1):
+                x = i / number_of_points
+                y = j / number_of_points
+                value = int(self.predict(np.array([x, y])))
+
+                color = colors[value]
+
+                plt.plot([x], [y], 'ro', color=color)
+
+        for index, input in enumerate(x_test):
+            color_value = int(self.predict(input))
+            plt.plot(input[0], input[1], 'ro', color=strong_colors[color_value])
+
+        medium_colors = ['#641E16', '#1B4F72', '#186A3B', '#AAB203', '#970297', '#974F02']
+
+        for index, input in enumerate(x_train):
+            color_value = int(self.predict(input))
+            plt.plot(input[0], input[1], 'ro', color=medium_colors[color_value])
+
+        plt.suptitle(title)
+        plt.show()
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from bayesian_classifier import Bayesian_classifier
 
-classifier = Bayesian_classifier()
 
-x_train, y_train, x_test, y_test, x_evaluation, y_evaluation = classifier.load_data('iris.data')
-classifier.fit(x_train, y_train)
+correct_rates = []
+datasets = ['iris.data', 'coluna.data', 'dermatology.data', 'breast-cancer.data', 'artificial.data']
+# datasets = ['artificial.data']
 
-accuracy = classifier.evaluate(x_test, y_test)
+for dataset in datasets:
 
-print("O classificador bayesiano acertou %.2f%%" % (accuracy * 100))
+    for realization in range(0, 1):
+
+        # print('Realização %d' % realization)
+
+        classifier = Bayesian_classifier()
+
+        x_train, y_train, x_test, y_test = classifier.load_data('Datasets/%s' % dataset)
+        classifier.fit(x_train, y_train)
+
+        print('Matriz de confusão para %s' % dataset)
+        correct_rate = classifier.evaluate(x_test, y_test, conf_matrix=True)
+        correct_rates.append(correct_rate)
+
+        # if realization == 0:
+        #     classifier.plot_decision_surface('Datasets/%s' % dataset, 'Datasets/%s' % dataset)
+
+    accuracy = sum(correct_rates)/len(correct_rates)
+    std = (sum([(x-accuracy)**2 for x in correct_rates])/len(correct_rates))**0.5
+
+    print("O classificador bayesiano puro acertou %.2f" % (accuracy*100))
+
+    print("O desvio padrão foi %.2f" % std)
